1686ade8b628575524a847bafe128f6e13b757c0
Jillo Woche <jillo.abdulla@gmail.com>
2025-08-21 21:45:23 +0300

    feat: add tailwindcss v4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-center items-center flex-col">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
