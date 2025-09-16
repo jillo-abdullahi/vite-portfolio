e8baa856221e2ff8abdb560ac5154283826fff3a
Jillo Woche <jillo.abdulla@gmail.com>
2025-09-16 11:06:34 +0300

    fix: initialize analytics correctly

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,15 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
 
 // Wait for the router to be ready
 await router.load();
 
+inject(); // Initialize Vercel Analytics
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-    <Analytics />
   </StrictMode>
 );
