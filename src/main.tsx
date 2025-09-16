1e108bad537cfa149b671f211db3ab45b74e2863
Jillo Woche <jillo.abdulla@gmail.com>
2025-09-16 10:57:42 +0300

    feat: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
+import { Analytics } from "@vercel/analytics/react";
 
 // Wait for the router to be ready
 await router.load();
@@ -10,5 +11,6 @@ await router.load();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </StrictMode>
 );
