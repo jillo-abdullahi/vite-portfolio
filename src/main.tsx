b0f89996bd632e8674032733d2c61d0be8445483
Jillo Woche <jillo.abdulla@gmail.com>
2025-09-16 11:13:02 +0300

    fix: update analytics and add speed insights as well

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import { createRoot } from "react-dom/client";
 import "@/index.css";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
-import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Wait for the router to be ready
 await router.load();
 
-inject(); // Initialize Vercel Analytics
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
+    <SpeedInsights />
   </StrictMode>
 );
